7259c6c0f677903163f5047c312602cbcb57c8c2
IhorShvydiuk <igor.shvidyuk@gmail.com>
2020-04-06 19:18:59 +0300

    added new solutions

---
+++ Advanced_Select/New_Companies.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT company_code
+       ,founder
+       ,COUNT(DISTINCT lead_manager_code)
+       ,COUNT(DISTINCT senior_manager_code)
+       ,COUNT(DISTINCT manager_code)
+       ,COUNT(DISTINCT employee_code)
+FROM Company
+     INNER JOIN Employee
+     USING(company_code)
+GROUP BY company_code, founder
+ORDER BY company_code;
