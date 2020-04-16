dc86868683d2c9281c5778c8579b9d44d44ab26e
IhorShvydiuk <igor.shvidyuk@gmail.com>
2020-04-16 12:10:36 +0300

    Update 15_Days_of_Learning_SQL.sql

--- Advanced_Join/15_Days_of_Learning_SQL.sql
+++ Advanced_Join/15_Days_of_Learning_SQL.sql
@@ -3,6 +3,7 @@ SELECT submission_date
        ,hacker_id
        ,name
 FROM   (
+        --we need only the first hacker who takes part in max num of submissions so row_num gives us that number
         SELECT submission_date
                ,cnt
                ,hacker_id
@@ -10,19 +11,23 @@ FROM   (
                ,cnt2
                ,ROW_NUMBER() OVER (PARTITION BY submission_date ORDER BY cnt2 DESC, hacker_id) rnmbr
         FROM   (
+               -- joined tables for looking the hacker id, name who takes part in max number of submissions per day
                 SELECT qtwo.submission_date
                        ,qtwo.cnt
                        ,s1.hacker_id
                        ,h.name
                        ,COUNT(s1.hacker_id) OVER (PARTITION BY s1.submission_date, s1.hacker_id ORDER BY s1.hacker_id) cnt2
                 FROM   (
+                       -- if day submission = row_num it means that hacker takes part each day and these hackers we need to count
                        SELECT  submission_date
                                ,COUNT(DISTINCT hacker_id) cnt
                        FROM    (
+                               --get data with ROW_NUMBER(). It is needed for comparing submission day with row_num
                                SELECT hacker_id
                                       ,submission_date
                                       ,ROW_NUMBER() OVER (PARTITION BY hacker_id ORDER BY submission_date) rhs
                                FROM   (
+                                       --get data without duplicates
                                        SELECT DISTINCT hacker_id
                                                        ,submission_date 
                                        FROM submissions
